867a3422ae3bbe3b5a9bceb46710a0b10d299208
yideshi <10000@tiyicn.cn>
2024-06-16 16:46:34 +0800

    测试

---
+++ module/pl_count.js
@@ -0,0 +1,11 @@
+// 数字专辑-新碟上架
+module.exports = (query, request) => {
+  const data = {}
+  return request('POST', `https://music.163.com/weapi/pl/count`, data, {
+    crypto: 'weapi',
+    cookie: query.cookie,
+    ua: query.ua || '',
+    proxy: query.proxy,
+    realIP: query.realIP,
+  })
+}
